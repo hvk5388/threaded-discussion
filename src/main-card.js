867e84a68331c7c91b45b8d343dc3333ec7af57c
Hannah <hvk5388@psu.edu>
2022-04-19 12:29:37 -0400

    not sure

--- src/main-card.js
+++ src/main-card.js
@@ -283,14 +283,15 @@ export class maincard extends SimpleColors {
     } return html`
       ${this.getComment({user_uid: "jim1234", likes: 69420, body: "Does I works?"}, "b")}
     
-      <div>
-        ${this.displayItems.map(
-          item => html`
-          ${this.getComment(item)}
-          `
-        )}
-      </div>
-    `;
+      `;
+      // <div>
+      //   ${this.displayItems.map(
+      //     item => html`
+      //     ${this.getComment(item)}
+      //     `
+      //   )}
+      // </div>
+
     
   }
 
